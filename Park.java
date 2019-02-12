93bb22356fa028ea8c88a0e455e7ea51163c78a8
DmytroHyba <44347983+DmytroHyba@users.noreply.github.com>
2019-02-12 10:52:07 +0200

    Update Park.java

--- Park.java
+++ Park.java
@@ -2,11 +2,11 @@ package com.company;
 
 public class Park {
 
-    public String address = "Zelena str.";
-    public int lenght = 5;
-    public int ticket_prize = 50;
-    private String name_trees = "red";
-    private String name_park = "Z1";
+    private String address = "Zelena str.";
+    private int lenght ;
+    private int ticketPrize;
+    private String name;
+    private String name1 = "Z1";
     protected String city = "Lviv";
     protected String country = "Ukraine";
     private static int EntranceTicketPrize = 0;
@@ -19,12 +19,12 @@ public class Park {
 
     }
 
-    public Park(String address, int lenght, int ticket_prize, String name_trees){
+    public Park(String address, int lenght, int ticketPrize, String name_trees){
 
         this.address = address;
         this.lenght = lenght;
-        this.ticket_prize = ticket_prize;
-        this.name_park = name_park;
+        this.ticketPrize = ticketPrize;
+        this.name = name;
     }
 
     public Park (String address,
@@ -34,29 +34,29 @@ public class Park {
                  String name_park,
                  String city,
                  String country) {
-        this(address, lenght, ticket_prize, name_trees);
-        this.name_park = name_park;
-        this.city = city;
-        this.country = country;
+        this.address = address;
+        this.lenght = lenght;
+        this.ticketPrize = ticket_prize;
+        this.name = name_park;
     }
 
-    public String getName_trees() {
+    public String getNameTrees() {
 
-        return name_trees;
+        return name;
     }
 
-    public void setName_trees(String name_trees) {
+    public void setNameTrees(String nameTrees) {
 
-        this.name_trees = name_trees;
+        this.name = nameTrees;
     }
 
     public String getName_park() {
-        return name_park;
+        return name;
     }
 
     public void setName_park(String name_park) {
 
-        this.name_park = name_park;
+        this.name = name_park;
     }
 
     public static void printStaticEntranceTicketPrize() {
@@ -73,11 +73,11 @@ public class Park {
         String information = "\nInformation about Park:\n";
         information += "Address:" + address + "\n";
         information += "Lenght:" + lenght + "\n";
-        information += "Ticket_prize:" + ticket_prize + "\n";
+        information += "Ticket_prize:" + ticketPrize + "\n";
         information +=  "Country:" + country + "\n";
-        information += "Name_park:" + name_park + "\n";
+        information += "Name_park:" + name + "\n";
         information += "City:" + city + "\n";
-        information += "Name_trees:" + name_trees;
+        information += "Name_trees:" + name;
         return information;
     }
     public void resetValues (String address,
@@ -90,10 +90,11 @@ public class Park {
 
         this.address = address;
         this.lenght = lenght;
-        this.ticket_prize = ticket_prize;
+        this.ticketPrize = ticket_prize;
         this.country = country;
-        this.name_park = name_park;
+        this.name = name_park;
         this.city = city;
-        this.name_trees = name_trees;
+        this.name = name_trees;
     }
 }
+
