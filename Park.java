c9cf7417b49177137becd4ec237c1e4667c7f4a9
DmytroHyba <44347983+DmytroHyba@users.noreply.github.com>
2019-02-13 09:46:14 +0200

    Update Park.java

--- Park.java
+++ Park.java
@@ -3,13 +3,13 @@ package com.company;
 public class Park {
 
     private String address = "Zelena str.";
-    private int lenght ;
-    private int ticketPrize;
+    private int lenght;
+    private int ticketPrice;
     private String name;
     private String name1 = "Z1";
     protected String city = "Lviv";
     protected String country = "Ukraine";
-    private static int EntranceTicketPrize = 0;
+    private static int EntranceTicketPrice = 0;
 
     public Park(String address) {
         this.address = address;
@@ -19,24 +19,24 @@ public class Park {
 
     }
 
-    public Park(String address, int lenght, int ticketPrize, String nameTrees){
+    public Park(String address, int lenght, int ticketPrice, String nameTrees) {
 
         this.address = address;
         this.lenght = lenght;
-        this.ticketPrize = ticketPrize;
+        this.ticketPrice = ticketPrice;
         this.name = name;
     }
 
-    public Park (String address,
-                 int lenght,
-                 int ticketPrize,
-                 String nameTrees,
-                 String namePark,
-                 String city,
-                 String country) {
+    public Park(String address,
+                int lenght,
+                int ticketPrize,
+                String nameTrees,
+                String name,
+                String city,
+                String country) {
         this.address = address;
         this.lenght = lenght;
-        this.ticketPrize = ticketPrize;
+        this.ticketPrice = ticketPrice;
         this.name = nameTrees;
     }
 
@@ -59,38 +59,39 @@ public class Park {
         this.name = namePark;
     }
 
-    public static void printStaticEntranceTicketPrize() {
+    public static void printStaticEntranceTicketPrice() {
 
-        System.out.println(EntranceTicketPrize + "ticket prize.");
+        System.out.println(EntranceTicketPrice + "ticket price.");
     }
 
-    public void printEntranceTicketPrize() {
+    public void printEntranceTicketPrice() {
 
-        System.out.println(EntranceTicketPrize + "ticket prize");
+        System.out.println(EntranceTicketPrice + "ticket price");
     }
 
     public String toString() {
         String information = "\nInformation about Park:\n";
         information += "Address:" + address + "\n";
         information += "Lenght:" + lenght + "\n";
-        information += "Ticket_prize:" + ticketPrize + "\n";
-        information +=  "Country:" + country + "\n";
+        information += "Ticket_price:" + ticketPrice + "\n";
+        information += "Country:" + country + "\n";
         information += "Name_park:" + name + "\n";
         information += "City:" + city + "\n";
         information += "Name_trees:" + name;
         return information;
     }
-    public void resetValues (String address,
-                              int lenght,
-                              int ticketPrize,
-                              String nameTrees,
-                              String namePark,
-                              String city,
-                              String country) {
+
+    public void resetValues(String address,
+                            int lenght,
+                            int ticketPrice,
+                            String nameTrees,
+                            String namePark,
+                            String city,
+                            String country) {
 
         this.address = address;
         this.lenght = lenght;
-        this.ticketPrize = ticketPrize;
+        this.ticketPrice = ticketPrice;
         this.country = country;
         this.name = namePark;
         this.city = city;
